388f67777602500b1fa3f5723dee45a72176dde7
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-01-31 23:17:50 +0300

    Add subscriptions

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
+  private igChangeSub: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -19,5 +21,9 @@ export class ShoppingListComponent implements OnInit {
         this.ingredients = ingredients;
       }
     )
+  }
+
+  ngOnDestroy(): void {
+    this.igChangeSub.unsubscribe();
   }
 }
